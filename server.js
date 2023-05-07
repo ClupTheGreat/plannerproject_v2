3d0cba0f961fe2c845fa488d4bc91232c31bda2e
ClupTheGreat <playclupfree@gmail.com>
2023-05-07 21:23:42 +0530

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
